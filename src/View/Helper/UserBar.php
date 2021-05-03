b6af00293914a16aad7914692f47ec68716ef0c5
Daniel Berthereau <Daniel.git@Berthereau.net>
2021-05-03 00:00:00 +0000

    Cleaned user bar.

--- src/View/Helper/UserBar.php
+++ src/View/Helper/UserBar.php
@@ -2,32 +2,19 @@
 
 namespace Guest\View\Helper;
 
-use Laminas\View\Helper\AbstractHelper;
-use Laminas\View\Renderer\RendererInterface;
-use Omeka\Api\Representation\SiteRepresentation;
-use Omeka\Entity\User;
-
 /**
  * View helper for rendering the user bar.
+ *
+ * Same of the Omeka user bar, except for guest users, who cannot go admin, and
+ * a default different link for the account.
  */
-class UserBar extends AbstractHelper
+class UserBar extends \Omeka\View\Helper\UserBar
 {
     /**
-     * The default partial view script.
-     */
-    const PARTIAL_NAME = 'common/user-bar';
-
-    /**
-     * The default partial view script.
+     * The default partial view script when the user has no right to go admin.
      */
     const PARTIAL_NAME_GUEST = 'common/user-bar-guest';
 
-    /**
-     * Render the user bar.
-     *
-     * @param string|null $partialName Name of view script, or a view model
-     * @return string
-     */
     public function __invoke($partialName = null)
     {
         $view = $this->getView();
@@ -48,13 +35,13 @@ class UserBar extends AbstractHelper
         }
 
         if ($user) {
-            $isGuest = $user->getRole() === \Guest\Permissions\Acl::ROLE_GUEST;
-            if ($isGuest) {
-                $links = [];
-                $partialName = $partialName ?: self::PARTIAL_NAME_GUEST;
-            } else {
+            $hasAdminRights = $view->userIsAllowed('Omeka\Controller\Admin\Index', 'index');
+            if ($hasAdminRights) {
                 $links = $this->links($view, $site, $user);
                 $partialName = $partialName ?: self::PARTIAL_NAME;
+            } else {
+                $links = [];
+                $partialName = $partialName ?: self::PARTIAL_NAME_GUEST;
             }
         } else {
             $links = [];
@@ -69,117 +56,5 @@ class UserBar extends AbstractHelper
                 'links' => $links,
             ]
         );
-    }
-
-    /**
-     * Prepare the list of links for the user bar.
-     *
-     * @param RendererInterface $view
-     * @param SiteRepresentation $site
-     * @param User $user
-     * @return array
-     */
-    protected function links(RendererInterface $view, SiteRepresentation $site, User $user)
-    {
-        if (!$view->userIsAllowed('Omeka\Controller\Admin\Index', 'index')) {
-            return [];
-        }
-
-        $links = [];
-        $translate = $view->plugin('translate');
-        $url = $view->plugin('url');
-
-        $links[] = [
-            'resource' => 'logo',
-            'action' => 'show',
-            'text' => $view->setting('installation_title', 'Omeka S'),
-            'url' => $url('admin'),
-        ];
-
-        $links[] = [
-            'resource' => 'site',
-            'action' => 'show',
-            'text' => $site->title(),
-            'url' => $site->adminUrl('show'),
-        ];
-
-        // There is no default label for resources, so get it from the controller (sometime upper-cased).
-        $params = $view->params();
-        $controller = strtolower((string) $params->fromRoute('__CONTROLLER__'));
-        $mapPluralLabels = [
-            'item' => 'Items', // @translate
-            'item-set' => 'Item sets', // @translate
-            'media' => 'Media', // @translate
-            'page' => 'Pages', // @translate
-        ];
-
-        if (!isset($mapPluralLabels[$controller])) {
-            return $links;
-        }
-
-        $routeParams = $params->fromRoute();
-        if ($controller === 'page') {
-            if ($routeParams['action'] === 'show') {
-                $links[] = [
-                    'resource' => $controller,
-                    'action' => 'browse',
-                    'text' => $translate($mapPluralLabels[$controller]),
-                    'url' => $url('admin/site/slug/action', ['site-slug' => $site->slug(), 'action' => 'page']),
-                ];
-                $page = empty($routeParams['page-slug'])
-                    ? $site->homepage()
-                    : $view->api()->searchOne('site_pages', ['site_id' => $site->id(), 'slug' => $routeParams['page-slug']])->getContent();
-                if ($page && $page->userIsAllowed('edit')) {
-                    $links[] = [
-                        'resource' => $controller,
-                        'action' => 'edit',
-                        'text' => $translate('Edit'),
-                        'url' => $page->adminUrl('edit'),
-                    ];
-                }
-            }
-        } else {
-            $action = $params->fromRoute('action');
-            $id = $params->fromRoute('id');
-
-            // Manage the special case for item set / show, routed as item / browse.
-            $itemSetId = ($controller === 'item' && $action === 'browse') ? $params->fromRoute('item-set-id') : null;
-            if ($itemSetId) {
-                $controller = 'item-set';
-                $action = 'show';
-                $id = $itemSetId;
-            }
-
-            $links[] = [
-                'resource' => $controller,
-                'action' => 'browse',
-                'text' => $translate($mapPluralLabels[$controller]),
-                'url' => $url('admin/default', ['controller' => $controller]),
-            ];
-
-            if ($id) {
-                $mapResourceNames = ['item' => 'items', 'item-set' => 'item_sets', 'media' => 'media'];
-                $resourceName = $mapResourceNames[$controller];
-                $resource = $view->api()->searchOne($resourceName, ['id' => $id])->getContent();
-                if ($resource) {
-                    $links[] = [
-                        'resource' => $controller,
-                        'action' => 'show',
-                        'text' => $translate('View'),
-                        'url' => $resource->adminUrl(),
-                    ];
-                    if ($resource->userIsAllowed('edit')) {
-                        $links[] = [
-                            'resource' => $controller,
-                            'action' => 'edit',
-                            'text' => $translate('Edit'),
-                            'url' => $resource->adminUrl('edit'),
-                        ];
-                    }
-                }
-            }
-        }
-
-        return $links;
     }
 }
