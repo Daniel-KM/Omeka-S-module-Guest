2c7d1a8e1850a9ef9c01fefb762e9720e3c0bdd6
Daniel Berthereau <Daniel.git@Berthereau.net>
2021-09-20 00:00:00 +0000

    Cleaned code.

--- src/Service/ControllerPlugin/UserSitesFactory.php
+++ src/Service/ControllerPlugin/UserSitesFactory.php
@@ -7,7 +7,7 @@ use Laminas\ServiceManager\Factory\FactoryInterface;
 
 class UserSitesFactory implements FactoryInterface
 {
-    public function __invoke(ContainerInterface $services, $requestedNamed, array $options = null)
+    public function __invoke(ContainerInterface $services, $requestedName, array $options = null)
     {
         return new UserSites(
             $services->get('Omeka\EntityManager')
