9e6e57197ab35e9768da73bd1518c35eb7ede32c
Daniel Berthereau <Daniel.git@Berthereau.net>
2023-07-10 00:00:00 +0000

    Released version 3.4.19.

--- data/scripts/upgrade.php
+++ data/scripts/upgrade.php
@@ -36,3 +36,28 @@ if (version_compare($oldVersion, '3.4.6', '<')) {
     $settings->set('guest_redirect', $guestRedirect === 'home' ? '/' : $guestRedirect);
     $settings->delete('guest_terms_redirect');
 }
+
+if (version_compare($oldVersion, '3.4.19', '<')) {
+    // Update existing tables.
+    $sqls = <<<'SQL'
+DROP INDEX guest_token_idx ON `guest_token`;
+DROP INDEX IDX_4AC9362FA76ED395 ON `guest_token`;
+DROP INDEX IDX_4AC9362F5F37A13B ON `guest_token`;
+ALTER TABLE `guest_token`
+    CHANGE `id` `id` INT AUTO_INCREMENT NOT NULL,
+    CHANGE `user_id` `user_id` INT NOT NULL AFTER `id`,
+    CHANGE `email` `email` VARCHAR(255) NOT NULL AFTER `user_id`,
+    CHANGE `token` `token` VARCHAR(255) NOT NULL AFTER `email`,
+    CHANGE `confirmed` `confirmed` TINYINT(1) NOT NULL AFTER `token`,
+    CHANGE `created` `created` DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL AFTER `confirmed`,
+    INDEX IDX_4AC9362FA76ED395 (`user_id`),
+    INDEX IDX_4AC9362F5F37A13B (`token`);
+ALTER TABLE `guest_token` ADD CONSTRAINT FK_4AC9362FA76ED395 FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE;
+SQL;
+    foreach (explode(";\n", $sqls) as $sql) {
+        try {
+            $connection->executeStatement($sql);
+        } catch (\Exception $e) {
+        }
+    }
+}
